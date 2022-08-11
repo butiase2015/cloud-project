fb4cab1cc5109f7d02986dc200e2d26b9892a1b3
“butiase2015” <butiase2015@gmail.com>
2022-08-11 17:23:09 +0000

    Creating fucntion for choice checking

--- code/newsapi.py
+++ code/newsapi.py
@@ -1,5 +1,37 @@
 import requests
 
+def checkChoice(choice):
+    if choice == "1":
+        query = str(input("I would like to search for news about... "))
+        return query
+
+    elif choice == "2":
+        query = str(input("I would like to search for news related to... "))
+        country =str(input("in the country of... "))
+        return query,country
+
+    elif choice == "3" :
+        print("Thansk for using our service ..... Bye")
+        
+        return (exit())
+
+
+print("Hey!!! Want some news")
+
+
+print("1. Search for news by Query")
+print("2. Search for news by Country and Topic")
+print("3. Exit")
+
+choice = str(input("I would like to pick... "))
+
+
+    
+
+
+    
+
+
 
 
 
